65126950c5134fa4333b32797557d1c67ae82421
Alexander Keliris <rigellute@gmail.com>
2018-04-03 22:24:15 +0100

    add scoped version of p5, make it work with parcel

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
+import p5 from 'p5';
 let n = 0;
 const c = 4;
 
-function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
-    angleMode(DEGREES);
-    background(50);
-    fill(255);
+function setup(sketch) {
+    return () => {
+        sketch.createCanvas(window.innerWidth, window.innerHeight);
+        sketch.angleMode(sketch.DEGREES);
+        sketch.background(50);
+        sketch.fill(255);
+    }
 }
 
-function draw() {
+function draw(sketch) {
+    return () => {
+        const a = n * 137.3;
+        const r = c * sketch.sqrt(n);
 
-    const a = n * 137.5;
-    const r = c * sqrt(n);
+        const x = r * sketch.cos(a) + sketch.width / 2;
+        const y = r * sketch.sin(a) + sketch.height / 2;
 
-    const x = r * cos(a) + width / 2;
-    const y = r * sin(a) + height / 2;
+        sketch.fill(n % 1000 + 130, n % 900, n % 256 + 50);
+        sketch.noStroke();
+        sketch.ellipse(x, y, 5, 5);
 
-    fill(n % 1000 + 130, n % 900, n % 256 + 50);
-    noStroke();
-    ellipse(x, y, 5, 5);
-
-    n++;
+        n++;
+    }
 }
+
+new p5(sketch => {
+    sketch.setup = setup(sketch);
+    sketch.draw = draw(sketch);
+})
